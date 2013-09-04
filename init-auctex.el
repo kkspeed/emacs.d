4a3822af781189c02f8e226a02c13a84872e4532
Bruce Li <leilmyxwz@gmail.com>
2013-09-04 11:40:54 -0400

    LaTeX support

--- init-auctex.el
+++ init-auctex.el
@@ -2,4 +2,8 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/auctex/preview")
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
+
+(setq TeX-engine "xetex")
+(setq LaTeX-command "xelatex")
+
 (provide 'init-auctex)
