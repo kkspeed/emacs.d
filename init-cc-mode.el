5f36c228c8e4ccc0adef87b977c9ba70d58dcfc6
Bruce Li <leilmyxwz@gmail.com>
2013-09-04 19:16:57 -0400

    Autocomplete <- disable yasnippet integration as it's not compatible

--- init-cc-mode.el
+++ init-cc-mode.el
@@ -4,16 +4,16 @@
 (require 'semantic)
 (require 'semantic/ia)
 
-(global-semantic-idle-completions-mode t)
+;(global-semantic-idle-completions-mode t)
 (global-semantic-decoration-mode t)
 (global-semantic-highlight-func-mode t)
 (global-semantic-show-unmatched-syntax-mode t)
 
-(add-hook 'c-mode-common-hook '(lambda()
-   (define-key c-mode-base-map (kbd "C-c j") 'semantic-ia-fast-jump)
-   (define-key c-mode-base-map (kbd "C-c b") 'semantic-mrub-switch-tags)
-   (define-key c-mode-base-map (kbd "C-x C-z") 'eassist-switch-h-cpp)))
-
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (define-key c-mode-base-map (kbd "C-c j") 'semantic-ia-fast-jump)
+             (define-key c-mode-base-map (kbd "C-c b") 'semantic-mrub-switch-tags)
+             (define-key c-mode-base-map (kbd "C-x C-z") 'eassist-switch-h-cpp)))
 
 ;; CC-mode
 (add-hook 'c-mode-hook '(lambda ()
@@ -21,5 +21,11 @@
                           (local-set-key (kbd "RET") 'newline-and-indent)
                           (semantic-mode t)
                           (semantic-idle-summary-mode +1)))
+
+(add-hook 'c++-mode-hook '(lambda ()
+                            (setq ac-sources (append '(ac-source-semantic-raw) ac-sources))
+                            (local-set-key (kbd "RET") 'newline-and-indent)
+                            (semantic-mode t)
+                            (semantic-idle-summary-mode +1)))
 
 (provide 'init-cc-mode)
